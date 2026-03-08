95b8d68c3074f9f84d858f84c66c9689dda56fea
lizeth-lm-07 <liz.murdocc@gmail.com>
2026-03-07 18:07:33 -0600

    Base de datos y htmls de las interfaces

--- Gestion_tickets/app.py
+++ Gestion_tickets/app.py
@@ -1,8 +1,8 @@
-import flask as Flask
 from flask import Flask, render_template
+import sqlite3
+from database import init_db
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -10,4 +10,14 @@ def index():
 
 
 if __name__ == '__main__':
+    init_db()        # crea la base de datos y tablas
     app.run(debug=True)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard_usuario.html')
+
+
+@app.route('/crear_ticket')
+def crear_ticket():
+    return render_template('crear_ticket.html')
